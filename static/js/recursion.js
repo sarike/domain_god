18ccec6b0dbc8bcba0092705eb0c6a7675051918
sarike <sarike@timefly.cn>
2014-01-10 18:50:32 +0800

    same string seg

---
+++ static/js/recursion.js
@@ -0,0 +1,70 @@
+var letters = 'abcdefghijklmnopqrstuvwxyz',
+	digitals = '0123456789';	
+
+String.prototype.times = function(n) {  
+    return Array.prototype.join.call({length:n+1}, this);  
+};  
+
+var multi_arr = function(origin_arr, multiple){
+	var result = [];
+	for (var i = 0; i < origin_arr.length; i++) {
+		result.push(origin_arr[i].times(multiple));
+	};
+	return result;
+};
+
+var domain_gen_pre = function(pattern){
+	var maybe_list = []
+	for (var i = 0; i < pattern.length; i++) {
+		var cur = pattern.charAt(i);
+		if(cur === 'w'){
+			maybe_list.push(letters.split(''));
+		}
+		if(cur === 'd'){
+			maybe_list.push(digitals.split(''));	
+		}
+		if(cur === '('){
+			var pre_bracket_index = i,
+				reg = /\(([wd]+)\)/;
+			var match = pattern.substring(pre_bracket_index).match(reg);
+			if(match){
+				var sub_pattern = match[1];
+				console.info(sub_pattern)
+				var sub_cur = sub_pattern.charAt(0);
+				if(sub_cur === 'w'){
+					maybe_list.push(multi_arr(letters.split(''), sub_pattern.length));
+				}
+				if(sub_cur === 'd'){
+					maybe_list.push(multi_arr(digitals.split(''), sub_pattern.length));	
+				}
+			}
+			
+			i += pattern.substring(pre_bracket_index).indexOf(')');
+		}
+	};
+	return maybe_list;
+};
+
+var domain_gen = function (maybe_list, start_index) {
+	var domain_list = [];
+
+	if(start_index === maybe_list.length - 1){
+		return maybe_list[start_index].split('');
+	}
+
+	var sub_domain_list = domain_gen(maybe_list, start_index + 1);
+
+	for (var i = 0; i < sub_domain_list.length; i++) {
+		domain_list.p
+	};
+
+	var cur_chars = maybe_list[start_index].split('');
+
+	for (var i = 0; i < cur_chars.length; i++) {
+		for (var j = 0; j < sub_domain_list.length; j++) {
+			domain_list.push(cur_chars[i] + sub_domain_list[j]);
+		};
+	};
+
+	return domain_list;
+};
--- static/js/wheel.js
+++ static/js/wheel.js
@@ -1,26 +1,32 @@
 var Wheel = function(options){
 	this.elements = options.elements || [];
-	this.on_circle = options.on_circle || function(){};
 	this.on_step = options.on_step || function(){};
 	this.name = options.name;
 
 	this.size = this.elements.length;
-	this.circles = 0;
 	this.position = 0;
 	this.over = false;
 
 	this.step = function(){
-		this.position = (this.position + 1) % this.size;
-		
-		if(this.postion === 0 && this.circles != 0 && this.next){
-			this.circles +=1;
-			this.next.step();
-		}
 		this.on_step();
+
+		this.position = (this.position + 1) % this.size;
+
+		if(this.position == this.size - 1 && !this.next){
+			if(!this.pre){
+				clearInterval(this.timer);
+				this.on_step();
+				return;
+			}
+			this.pre.next = null;
+		}
+		
+		if(this.position === this.size - 1 && this.next){
+			this.next.step();	
+		}
 	};
 
 	this.value = function(){
-		console.info(this.name + " : " + this.position)
 		return this.elements[this.position];
 	};
 
@@ -35,14 +41,15 @@ var Wheel = function(options){
 var WheelContainer = {
 	wheels: [],
 
-	init: function(){
-		for (var i = 0; i < 10; i++) {
+	init: function(maybe_list){
+		for (var i = 0; i < maybe_list.length; i++) {
 			var w = new Wheel({
-				elements: '0123456789'.split(''),
+				elements: maybe_list[i],
 				name: 'wheel_' + i
 			})
 			if(i!=0){
 				this.wheels[i-1].next = w;
+				w.pre = this.wheels[i-1];
 			}
 			this.wheels.push(w);
 		}
@@ -50,19 +57,13 @@ var WheelContainer = {
 
 	value: function () {
 		var result = "";
-		for (var i = this.wheels.length - 1; i >= 0; i--) {
+		for (var i = 0; i < this.wheels.length; i++) {
 			result += this.wheels[i].value();
 		};
 		return result;
 	},
 
 	go: function(){
-		this.init();
-		console.info(this.wheels);
-		var self = this;
-		this.wheels[0].on_step=function () {
-			console.info(self.value());
-		}
-		this.wheels[0].run();
+		this.wheels[0].run(100);
 	}
 };
